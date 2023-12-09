b5eb1a49e054a14d9063a72a2d2fa8f0016c3023
Leah Amelia Chen <hi@pluie.me>
2023-12-10 00:41:04 +0100

    day 8 part 2 - lcm solution!

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,5 +1,8 @@
 module Day8 where
 
+import Control.Monad (liftM2)
+import Data.List (elemIndex, findIndex)
+import Data.Maybe (fromMaybe)
 import Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as Map
 import Test.Hspec (Spec, describe)
@@ -12,51 +15,50 @@ test = describe "Day 8" $ do
   example 8 1 1 $ parseShouldBe 2 solnA
   example 8 1 2 $ parseShouldBe 6 solnA
   input 8 1 $ parseShouldBe 22411 solnA
-  example 8 2 2 $ parseShouldBe 6 solnB
-  input 8 2 $ const unimplemented
+  example 8 2 3 $ parseShouldBe 6 solnB
+  input 8 2 $ parseShouldBe 11188774513823 solnB
 
+walk
+  :: (String -> Bool)   -- end criterion
+  -> Map String Node    -- map of nodes
+  -> [Direction]        -- list of directions, should be infinite
+  -> String             -- starting node
+  -> Int
+walk f m = walk' 0
+  where
+    pickDir DLeft = left
+    pickDir DRight = right
+    walk' i (d:ds) cur
+      | f cur = i
+      | otherwise = walk' (i + 1) ds $ pickDir d $ m ! cur
+
+data Node = Node { left :: String, right :: String }
 data Direction = DLeft | DRight deriving (Eq, Show)
-
-walkA :: [Direction] -> Map String Node -> Int -> String -> Int
-walkA _ _ i "ZZZ" = i
-walkA (d:ds) map i cur =
-  walkA ds map (i + 1) $ case d of
-    DLeft -> l
-    DRight -> r
-  where (l, r) = map ! cur
-
-walkB :: [Direction] -> Map String Node -> Int -> [String] -> Int
-walkB (d:ds) m i curs =
-  if all (\c -> last c == 'Z') curs then
-    i
-  else
-    walkB ds m (i + 1) $ map (case d of
-      DLeft -> fst . (!) m
-      DRight -> snd . (!) m) curs
-
-type Node = (String, String)
 
 solnA :: Parser Int
 solnA = do
-  (dirs, map) <- parseInput
-  return $ walkA (cycle dirs) map 0 "AAA"
+  (ds, m) <- parseInput
+  return $ walk (== "ZZZ") m ds "AAA"
 
 solnB :: Parser Int
 solnB = do
-  (dirs, map) <- parseInput
-  return $ walkB (cycle dirs) map 0 $
-    filter (\c -> last c == 'A') (Map.keys map)
+  (ds, m) <- parseInput
+  return $
+    foldl lcm 1 $
+    map (walk ((==) 'Z' . last) m ds) $
+    filter ((==) 'A' . last) $
+    Map.keys m
 
 parseInput :: Parser ([Direction], Map String Node)
 parseInput = (,)
-  <$> some dir <* newline <* newline
+  <$> (cycle <$> some dir <* newline <* newline)
   <*> (Map.fromList <$> node `sepEndBy` newline)
   where
     dir = DLeft <$ char 'L' <|> DRight <$ char 'R'
-    label = some (digitChar <|> upperChar)
+    label = some $ digitChar <|> upperChar
     node = (,)
       <$> label <* hspace <* char '=' <* hspace
       <*> between (char '(') (char ')') (
-        (,) <$> label <* string ", " <*> label
+        Node <$> label <* string ", " <*> label
       )
 
